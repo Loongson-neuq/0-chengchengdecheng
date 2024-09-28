a8c3d9e4f0945a7435b6ae74cce23a7bfed5523f
Boyao Feng <3061715126@qq.com>
2024-09-28 22:58:02 +0800

    完成作业“ git commit -m 完成作业”

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include ".assignment/test.h"
+#include <algorithm>
 
 // 比较三个数的大小
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
-    // 在这里完成你的代码
+    result.min = a;
+    result.mid = b;
+    result.max = c;
+
+    std::sort(&result.min,&result.max + 1);
 }
